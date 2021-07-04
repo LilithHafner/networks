d3ace594ac97b9626e2a1849d73ad723233f8ab4
Lilith Orion Hafner <lilithhafner@gmail.com>
2021-07-04 15:58:11 -0500

    Extend special case support to 3d, start general case (both slower)

--- unrank.jl
+++ unrank.jl
@@ -11,6 +11,46 @@ function unrank2d(n)
     a,b
 end
 
+function unrank3d(n)
+    # Roughly O(1) for n :: Int64.
+    # Constant: 2ns = sqrt runtime +5% -0%
+    # Max input: 2^(NN-2)-1 for IntNN
+    a = Int(floor((6n)^(1/3)))
+    n -= (a-1)*a*(a+1)÷6
+    b = Int(round(sqrt(2n)))
+    c = n-(b*(b-1))>>1
+    #Depending on benchmark,
+    # >>1 makes unrank2d 1.1-5x faster than ÷2.
+    a,b,c
+end
+
+function unrank(d,n)
+    #=if d == 1
+        return [n]
+    elseif d == 2
+        return unrank2d(n)
+    elseif d == 3
+        return unrank3d(n)
+    end=#
+    unrank!(Vector{typeof(n)}(undef, d), d,n)
+end
+function unrank!(array, d,n)
+    if d == 1
+        array[d] = n
+        return array
+    end
+    if d == 2
+        a = Int(round(sqrt(2n)))
+        array[d] = a
+        return unrank!(array, 1, n -(a*(a-1))>>1)
+    end
+    if d == 3
+        a = Int(floor((6n)^(1/3)))
+        array[d] = a
+        return unrank!(array, 2, n - (a-1)*a*(a+1)÷6)
+    end
+end
+
 ### unrank2d Tests
 #=
 println(collect(unrank2d.(1:10)))
