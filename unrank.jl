e37d111277d4e52a1b6ed43efd53cc2e2c00b33c
Lilith Orion Hafner <lilithhafner@gmail.com>
2021-07-04 12:44:00 -0500

    Switched to 1-indexing

--- unrank.jl
+++ unrank.jl
@@ -4,25 +4,32 @@ function unrank2d(n)
     # Roughly O(1) for n :: Int64.
     # Constant: 2ns = sqrt runtime +5% -0%
     # Max input: 2^(NN-2)-1 for IntNN
-    a = Int(round(sqrt(2n+1)-1))
-    b = n-(a*(a+1))>>1
+    a = Int(round(sqrt(2n)))
+    b = n-(a*(a-1))>>1
     #Depending on benchmark,
-    # >>1 makes unrank2 1.1-5x faster than ÷2.
+    # >>1 makes unrank2d 1.1-5x faster than ÷2.
     a,b
 end
 
 ### unrank2d Tests
 #=
+println(collect(unrank2d.(1:10)))
+@assert collect(unrank2d.(1:10)) == [(1, 1), (2, 1), (2, 2), (3, 1), (3, 2),
+                                     (3, 3), (4, 1), (4, 2), (4, 3), (4, 4)]
+@assert unrank2d(87109723841) == (417396, 222131)
+
+#@benchmark unrank2d(9720290439024)
+
 function test(n)
     try
         @assert typeof(n) <: Integer
-        @assert n >= 0
+        @assert n >= 1
         a,b = unrank2d(n)
-        @assert a >= 0
-        @assert b >= 0
+        @assert a >= 1
+        @assert b >= 1
 
         @assert b <= a
-        @assert a*(a+1)÷2+b == n
+        @assert a*(a-1)÷2+b == n
     catch
         println(typeof(n))
         println(n)
@@ -39,10 +46,7 @@ function plt(x)
     scatter(x,t)
 end
 
-plot(plt(0:10^3),
-     plt(2^62-1:-(2^62-1)÷10^3:0),
+plot(plt(1:10^2),
+     plt(2^62-1:-(2^62-1)÷10^2:1),
     layout=(2,1))
-
-println(collect(unrank2d.(0:10)))
-@benchmark unrank2d(9720290439024)
 =#
