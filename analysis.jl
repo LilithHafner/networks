86de487d0f0a662f6b24a8555d4398b3e45cea5e
Lilith Orion Hafner <lilithhafner@gmail.com>
2021-07-12 16:03:38 -0500

    Poke at some properties of snake model

--- analysis.jl
+++ analysis.jl
@@ -1,8 +1,71 @@
 #TODO include anything worth keeping from statistics.jl
 
+##Fast O(size)
+function clustering_coefficients(edge_list::Vector{Vector{T}}) where T <: Any
+    #Θ(size)
+    #Computes a nearly uniform and not independent sample.
+    #Non-uniformity is at most 1 node.
+    #Approximates the average local and the global clustering coefficients
+
+    size = 0
+    n2e = Dict{T,Vector{Set{T}}}() #Θ(size)
+    for edge ∈ edge_list
+        size += length(edge)
+        for n ∈ edge
+            if n ∈ keys(n2e)
+                push!(n2e[n], Set(edge))
+            else
+                push!(n2e, n=>[Set(edge)])
+            end
+        end
+    end
+
+
+    #Each triangle contains 3 closed wedges
+    fuel = size + 100
+    wedges, closed_wedges = 0,0#For global clustering coefficient
+    lcc_nan = []#For local clustering coefficient
+    nodes = collect(keys(n2e))
+    for n ∈ shuffle(nodes)# Θ(fuel) = θ(size)
+        fuel <= 0 && break
+        local_closed_wedges, local_wedges = 0,0
+        edges = shuffle(collect(setdiff(e, n) for e in n2e[n]))
+        for (i,e1) ∈ enumerate(edges)
+            for e2 ∈ edges[i+1:length(edges)]
+                local_wedges += 1
+                fuel -= length(e2)
+                for n1 ∈ e1
+                    for e11 ∈ n2e[n1]
+                        if !isdisjoint(e11, e2)#TODO change documentation for isdisjoint in Julia.
+                            local_closed_wedges += 1
+                            @goto break_label
+                        end
+                    end
+                end
+                @label break_label
+            end
+        end
+        wedges += local_wedges
+        closed_wedges += local_closed_wedges
+        push!(lcc_nan, local_closed_wedges/local_wedges)
+    end
+    lcc = filter!((!) ∘ isnan, lcc_nan)
+    Dict{Symbol,Any}(
+        :gcc => closed_wedges/wedges,
+        :lcc => lcc,
+        :lcc_nan => lcc_nan,
+        :acc => sum(lcc)/length(lcc),
+        :wedges => wedges*length(nodes)/length(lcc_nan),
+        :triangles => closed_wedges*length(nodes)/length(lcc_nan)/3,
+        :sample_nodes => length(lcc_nan),
+        :sample_proportion => length(lcc_nan)/length(nodes),
+    )
+end
+
+##Slow
 function wedge_distribution(edges::AbstractVector{E}) where {N <: Any, E <: AbstractVector{N}}
+    #O(k^2*Σdegree^2) ~ O(k^2dm)
     #This could but does not use concepts of relationship to self.
-    #This is very slow O(k^2*Σdegree^2)
     edges_by_node = Dict{N,Set{E}}()
     nodes_by_node = Dict{N,Set{N}}()
     for edge ∈ edges
--- snake_model_plots.jl
+++ snake_model_plots.jl
@@ -5,31 +5,94 @@ using Plots
 using Random: shuffle
 
 cache = Dict()
-function sm(args...; trial=1)
-    key = (args, trial)
+function generate(f, args...; trial=1)
+    key = (f, args, trial)
     if haskey(cache, key)
         return cache[key]
     end
-    result_tuple = @timed snakemodel1(args...)
+    result_tuple = @timed f(args...)
     result = Dict(zip(keys(result_tuple), result_tuple))
     result[:graph] = result[:value]
-    @time result[:wedges],result[:lcc] = wedge_distribution(result[:graph])
-    #result[:lcc_nonzero] = (sum(wd)-wd[1])/sum(wd)
-    #result[:lcc_mean] = sum((c-1)*wd[c] for c ∈ eachindex(wd))/sum(wd)
+    merge!(result, clustering_coefficients(result[:graph]))
     cache[key] = result
 end
 
+
+    x,y = [],[]
+    for trial in 1:2
+        for width in shuffle(1:128)
+            speed = .2
+            sz = 40
+            result = generate(snakemodel2,
+                sz*[9,16,50,120,20],
+                sz*[5,10,30,30,30,20,20,10,4,1],
+                width, speed, trial=trial)
+            push!(x, width)
+            push!(y, result)
+        end
+    end
+    xy = zip(x,y)
+    xs = sort(collect(Set(x)))
+
+#scatter(xlim=(0,1), ylim=(0,1))
+#scatter(ylim=(0,100))
+scatter()
+
+scatter!(x, [y[:acc] for y in y], label="acc", ma=.1)
+scatter!(x, [y[:gcc] for y in y], label="gcc", ma=.1)
+plot!(xs, [mean(y[:acc] for (x,y) in xy if x == xi) for xi in xs], label="acc mean")
+plot!(xs, [mean(y[:gcc] for (x,y) in xy if x == xi) for xi in xs], label="gcc mean")
+
+#end
+
+#scatter!(s1, x, [i[:lcc_mean] for i in y], label="llc mean")
+#s2 = scatter(x, [i[:time] for i in y]*1e3, label="time (ms)")
+#g = grasshop_populate([100], [1,1,1,1], 10^3/binomial(100+4-1,4))
+
+## Time
+#=
+
+#for width in [1,2,4,8,16,32,64,128]
+#println(width)
+width = 20
+speed = .2
 x,y = [],[]
-for trial in 1:1
-    for speed in shuffle(0:.025:1)
-                  # k, node, edge, width, sp, dupli, trial
-        result = sm(4, 1000, 1000, 20, speed, false, trial=trial)
-        push!(x, speed)
-        push!(y, result)
+#runs = reduce(vcat, [[(trial,sz) for trial in 1:500÷sz] for sz in 1:10:500])
+#for (trial, sz) in shuffle(runs)
+for trial in 1:5
+    for sz in shuffle(1:4:50)#0:.025:1)
+                                     # k, node, edge, width, sp, dupli, trial
+        #result = generate(snakemodel1, 4, 1000, 1000, 20, speed, false, trial=trial)
+        result = generate(snakemodel2,
+            sz*[9,16,50,120,20],
+            sz*[5,10,30,30,30,20,20,10,4,1],
+            width, speed, trial=trial)
+        push!(x, sum(length.(result[:graph])))
+        push!(y, result[:time]*1000)
     end
 end
-s1 = scatter()
-scatter!(s1, x, [i[:lcc] for i in y], label="lcc")
+xy = zip(x,y)
+sx = sort(collect(Set(x)))
+
+scatter(xlim=(0,1), ylim=(0,1))
+scatter(ylim=(0,100))
+scatter()
+plot!(sx,
+    [mean(y for (x,y) in xy if x == xi) for xi in sx],
+    label="mean")
+my = [minimum(y for (x,y) in xy if x == xi) for xi in sx]
+plot!(sx, my, label="minimum", legend=false)
+plot!([0,maximum(sx)], [0, maximum(sx)*sum(my.*sx)/sum(sx.*sx)], label="minimum", legend=false)
+
+plot!(sx,
+    [median(y for (x,y) in xy if x == xi) for xi in sx],
+    label="median")
+scatter!(x, y, label="time (ms)", smooth=true)
+
+#end
+
+s1
 #scatter!(s1, x, [i[:lcc_mean] for i in y], label="llc mean")
-s2 = scatter(x, [i[:time] for i in y]*1e3, label="time (ms)")
+#s2 = scatter(x, [i[:time] for i in y]*1e3, label="time (ms)")
 #g = grasshop_populate([100], [1,1,1,1], 10^3/binomial(100+4-1,4))
+=#
